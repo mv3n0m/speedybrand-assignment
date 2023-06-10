fc5db0540f088e2b7284cbe46fb6a14f023e25dc
Manish Roy <manish.11612939@lpu.in>
2023-06-10 18:26:19 +0530

    introduced additional props for Btn component

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 
 function Btn(props) {
-    const { text, onClick, className } = props
+    const { text, onClick, className, disabled } = props
 
     return (
-        <button onClick={ onClick } className={ `text-center bg-orange-600 w-[140px] px-4 py-2 text-white rounded-md ${ className }` }>
+        <button onClick={ onClick }
+            className={ `text-center ${ disabled ? "bg-zinc-300 text-zinc-400" : "bg-orange-600 text-white" } w-[140px] px-4 py-2 rounded-md ${ className }` }
+            disabled={ disabled }
+        >
             { text || props.children }
         </button>
     )
