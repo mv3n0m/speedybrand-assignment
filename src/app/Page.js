191cc91a5867b9c17edf54d2089de9d8f2831be3
Manish Roy <manish.11612939@lpu.in>
2023-06-10 16:52:04 +0530

    created Add Topic button

--- src/app/Page.js
+++ src/app/Page.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 import Categories from './Categories'
+import { Btn } from 'components'
+import { BiMessageSquareAdd } from 'react-icons/bi'
 
 
 function Page() {
     const [ activeCategory, setActiveCategory ] = useState(null)
+    const [ modalMode, setModalMode ] = useState(null)
+
 
     return (
         <div className='py-5 px-10'>
-            <Categories activeCategory={ activeCategory } setActiveCategory={ setActiveCategory } />
+            <div className='flex border-b justify-between items-center'>
+                <Categories activeCategory={ activeCategory } setActiveCategory={ setActiveCategory } />
+                <Btn onClick={() => setModalMode("addTopic")} className="mt-5 flex gap-2 items-center justify-between">
+                    <BiMessageSquareAdd size={ 20 }/>
+                    Add Topic
+                </Btn>
+            </div>
         </div>
     )
 }
