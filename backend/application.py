e6fff651c1bc9a1f2a55db0f0f752b5f9e7054ce
Manish Roy <manish.11612939@lpu.in>
2023-06-11 22:12:30 +0530

    added generate-blog route (dummy contents)

--- backend/application.py
+++ backend/application.py
@@ -1,11 +1,11 @@
 import os
+import time
 from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 from mongo import MongoWrapper
 from utils import responsify
 from bson.objectid import ObjectId
-
 
 load_dotenv()
 
@@ -38,12 +38,27 @@ def delete_topic():
     request_json = request.json
 
     if "_id" not in request_json:
-        return responsify({error: "_id is a required field"}, 400)
+        return responsify({"error": "_id is a required field"}, 400)
 
     _id = ObjectId(request_json["_id"])
 
     db.delete("topics", { "_id": _id })
     return responsify({"success": "Topic deleted successfully."}, 200)
+
+
+@application.route("/generate-blog", methods=["POST"])
+def generate_blog():
+    request_json = request.json
+
+    # simulating request time
+    time.sleep(2)
+
+    content = db.get("blogs", request_json)
+    if not content:
+        return responsify({"error": "No blog can be generated / Invalid tone value."}, 400)
+
+    content = content[0]["content"]
+    return responsify(content, 200)
 
 
 if __name__ == "__main__":
